5c1e3c352ab15e4d40e370ea99944d8446c3004c
chand@CS <sharmach66@gmail.com>
2026-04-15 11:56:32 +1000

    added test files

---
+++ test1.py
@@ -0,0 +1,7 @@
+print("this is testing2.py")
+print("this is my second code")
+for i in range(5):
+    print(i)
+    print("this is the end of the loop")
+    print("this is the end of the code")
+    
